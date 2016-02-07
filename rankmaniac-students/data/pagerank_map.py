f6e50d178b80b1269f9c05e6647c74358bda1efe
Bolton Bailey <bolton.bailey@gmail.com>
2016-02-07 15:26:16 -0800

    Added code to pagerank

--- rankmaniac-students/data/pagerank_map.py
+++ rankmaniac-students/data/pagerank_map.py
@@ -7,4 +7,26 @@ import sys
 #
 
 for line in sys.stdin:
+
+    parsed_key_value = line.split("\t")
+
+    assert len(parsed_key_value) == 2
+
+    parsed_key = parsed_key_value[0]
+    assert parsed_key.startswith("NodeId:")
+
+    node_id = int(parsed_key[len("NodeId:"):])
+
+    parsed_value = parsed_key_value[1].split(",")
+
+    current_rank = float(parsed_value[0])
+
+    previous_rank = float(parsed_value[1])
+
+    links = map(int, parsed_value[2:])
+
+
+    for neighbor_id in links:
+    	sys.stdout.write("NodeId:" + str(neighbor_id) + "\t" + str(current_rank/len(links)) + "\n")
+
     sys.stdout.write(line)
--- rankmaniac-students/data/pagerank_reduce.py
+++ rankmaniac-students/data/pagerank_reduce.py
@@ -6,6 +6,63 @@ import sys
 # This program simply represents the identity function.
 #
 
+node_id_new_rank = dict()
+node_id_old_rank = dict()
+node_id_links = dict()
+
+ALPHA = 0.85
+
 for line in sys.stdin:
-    sys.stdout.write(line)
+    
+    parsed_key_value = line.split("\t")
+
+    assert len(parsed_key_value) == 2
+
+    parsed_key = parsed_key_value[0]
+
+    node_id = int(parsed_key[len("NodeId:"):])
+
+    parsed_value = parsed_key_value[1].split(",")
+
+    if len(parsed_value) == 1:
+
+        parsed_rank = float(parsed_value[0])
+
+        if node_id not in node_id_new_rank:
+            node_id_new_rank[node_id] = 0.0
+
+        node_id_new_rank[node_id] += parsed_rank
+
+    else:
+        current_rank = float(parsed_value[0])
+
+        node_id_old_rank[node_id] = current_rank
+
+        links = parsed_value[2:]
+
+        node_id_links[node_id] = links
+
+
+for node_id in node_id_new_rank:
+
+    output = ""
+    output += "NodeId:"
+    output += str(node_id)
+    output += "\t"
+    output += str(node_id_new_rank[node_id])
+    output += ","
+    output += str(node_id_old_rank[node_id])
+    output += ","
+    output += ",".join(node_id_links[node_id])
+
+    sys.stdout.write(output)
+
+
+
+
+
+
+
+
+
 
